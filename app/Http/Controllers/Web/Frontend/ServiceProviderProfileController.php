9de1f75e4be821d2875bdd3d8ad8d3fe0054e95f
hafizurrahmanshadhin <hr.shadhin36@gmail.com>
2025-02-02 18:31:50 +0600

    Beauty Expert profile complete

--- app/Http/Controllers/Web/Frontend/ServiceProviderProfileController.php
+++ app/Http/Controllers/Web/Frontend/ServiceProviderProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Web\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Review;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -15,6 +16,11 @@ class ServiceProviderProfileController extends Controller {
      */
     public function index(Request $request, $userId, $serviceId): View {
         $user = User::with(['userServices.service'])->findOrFail($userId);
-        return view('frontend.layouts.service_provider_profile.index', compact('user', 'serviceId'));
+
+        $reviews = Review::whereHas('booking.userService', function ($query) use ($userId) {
+            $query->where('user_id', $userId);
+        })->with(['booking.userService'])->get();
+
+        return view('frontend.layouts.service_provider_profile.index', compact('user', 'serviceId', 'reviews'));
     }
 }
