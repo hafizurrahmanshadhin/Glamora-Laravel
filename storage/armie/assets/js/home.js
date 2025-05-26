0855577beffde4de88d6ab91f314c51c0857142c
hafizurrahmanshadhin <hr.shadhin36@gmail.com>
2025-05-26 15:47:23 +0600

    ...

---
+++ storage/armie/assets/js/home.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Initialize Flatpickr on the input element
+    const dateInput = document.getElementById('date-input');
+    const flatpickrInstance = flatpickr(dateInput, {
+      dateFormat: "d/m/y",
+      minDate: "today" // Disable past dates
+    });
+
+    // Add event listener to open Flatpickr on container click
+    document.querySelector('.date-picker-container').addEventListener('click', function() {
+      flatpickrInstance.open();
+    });
+  });
+
+  function copyMarqueeNode() {
+    var heroMarquee = document.querySelectorAll(".rk--hero--marquee .slide");
+    var ParentContainer = document.querySelectorAll(".rk--hero--marquee");
+
+    var storiesMarqueeWrapper = document.querySelectorAll(
+      ".stories--marquee--slider"
+    );
+
+    if (heroMarquee) {
+      ParentContainer.forEach((container) => {
+        heroMarquee.forEach((marquee) => {
+          let copySlide = marquee.cloneNode(true);
+          container.appendChild(copySlide);
+        });
+      });
+    }
+
+    if (storiesMarqueeWrapper) {
+      storiesMarqueeWrapper.forEach((storiesMarquee) => {
+        var copyStroriesSlide = storiesMarquee
+          .querySelector(".slide")
+          .cloneNode(true);
+        storiesMarquee.appendChild(copyStroriesSlide);
+      });
+    }
+  }
+  copyMarqueeNode();
